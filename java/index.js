ea64bb2ff9e0900234803c1ca9d843a8476c4246
Mattykea <math764n@stud.kea.dk>
2021-12-14 13:09:13 +0100

    eh

--- java/index.js
+++ java/index.js
@@ -3,14 +3,11 @@ const btn = document.querySelector(".toggle-btn");
 // Lav en variabel, der refererer til "nav"
 const nav = document.querySelector("navbar");
 
-
 // Lav en function, der hedder toggleMenu()
 function toggleMenu() {
-
   // 1. Toggle en klasse på nav vha. classList.toggle
   // 2. Toggle en klasse, der hedder "shown"
   nav.classList.toggle("shown");
-
 
   // 1. Lav en variabel, der hedder menuShown
   // 2. Den skal være lig med, om nav-variablen indeholder klassen "shown" vha. classList.contains("")
@@ -20,7 +17,7 @@ function toggleMenu() {
   // 2. Spørg om menuShown i if-sætningen => if (menu)
   if (menuShown) {
     // hvis nav har klassen "shown", sæt da btn.textContent til "Luk"
-    document.querySelector(".toggle-btn").textContent = "X"
+    document.querySelector(".toggle-btn").textContent = "X";
   } else {
     // hvis IKKE nav har klassen "shown", sæt da btn.textContent til "Menu"
     btn.textContent = "☰";
